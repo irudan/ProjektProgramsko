a9082a1bf30394ff0bc5ea52f906a6be314a83b7
Valentina Devald <vdevald@unipu.hr>
2017-01-07 23:27:41 +0100

    Dodani presenteri za Casopis
    Dodani presenteri za Casopis: CasopisNode i CasopisNodeStore

---
+++ ProjektProgramsko/Presenter/CasopisNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ProjektProgramsko
+{
+	public class CasopisNode : Gtk.TreeNode
+	{
+		[Gtk.TreeNodeValue(Column = 0)]
+		public String naziv;
+
+
+		[Gtk.TreeNodeValue(Column = 1)]
+		public String tagovi;
+
+		public CasopisNode(Casopis c)
+		{
+			this.naziv = c.Naziv;
+			this.tagovi = c.Tagovi;
+		}
+
+	}
+}
---
+++ ProjektProgramsko/Presenter/CasopisNodeStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjektProgramsko
+{
+	public class CasopisNodeStore : Gtk.NodeStore
+	{
+		public CasopisNodeStore() : base(typeof(CasopisNode))
+		{
+		}
+
+		public void dodajCasopis(Casopis c)
+		{
+			CasopisNode temp = new CasopisNode(c);
+			this.AddNode(temp);
+		}
+
+		public void dodajCasopis(List<Casopis> c)
+		{
+			foreach (Casopis i in c)
+			{
+				CasopisNode temp = new CasopisNode(i);
+				this.AddNode(temp);
+			}
+		}
+	}
+}
